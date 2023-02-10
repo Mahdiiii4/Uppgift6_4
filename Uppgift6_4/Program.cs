6da99481e843c0ca2995e7c60a8ed486707bf07a
Alexseptt100 <alan03002@utb.vaxjo.se>
2023-02-10 10:26:51 +0100

    Add project files.

---
+++ Uppgift6_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uppgift6_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            upphöjd(2);
+        }
+        static void upphöjd(int tal)
+        {
+            tal = tal * tal;
+            Console.WriteLine("Den resultat är: " +tal+ ".");
+        }
+    }
+}
