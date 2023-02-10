dbb08a005065d64aa332f353f1044d592db739d4
Mahdiiii4 <111962604+Mahdiiii4@users.noreply.github.com>
2023-02-10 10:53:37 +0100

    Update Program.cs

--- Uppgift6_4/Program.cs
+++ Uppgift6_4/Program.cs
@@ -1,19 +1,31 @@
 using System;
+using System.Data.Common;
 
-namespace uppgift6_4
+namespace uppgift6_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Skriv ner nummert");
+            Console.WriteLine("Skriv ner var en nummer på var sin rad");
             int nummer = int.Parse(Console.ReadLine());
-            upphöjd(nummer);
+            int nummer2 = int.Parse(Console.ReadLine());
+            Störst(nummer, nummer2);
         }
-        static void upphöjd(int tal)
+        static void Störst(int tal1, int tal2)
         {
-            tal = tal * tal;
-            Console.WriteLine("Den resultat är: " + tal + ".");
+            if (tal1 > tal2)
+            {
+                Console.WriteLine("Den största talen är" + tal1 + ".");
+            }
+            else if (tal1 < tal2)
+            {
+                Console.WriteLine("Den största talen är" + tal2 + ".");
+            }
+            else if (tal1 == tal2)
+            {
+                Console.WriteLine("De är lika stora");
+            }
         }
     }
 }
